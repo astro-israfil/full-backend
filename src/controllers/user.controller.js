e433d6f6cd0cfe7bbd2c9ff983996a40707405b3
astro-israfil <astro.israfil.fi@gmail.com>
2024-10-20 18:47:03 +0600

    add user register route

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+import asyncHandler from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async function (req, res) {
+  res.status(200).json({ message: "success" });
+});
+
+export { registerUser };
