32a29b74bdae7cd60d129f08dbdeabb50fa63557
astro-israfil <astro.israfil.fi@gmail.com>
2024-10-20 12:55:30 +0600

    add express middlewares and utility functions to handler async functions and make custom error and response class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "../.env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("An error event of", error);
+    });
+    app.listen(port, () => {
+      console.log("Application running on port:", port);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+  });
